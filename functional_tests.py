63805c7eddd6055c921ff4ef1331f7a3584e508c
Chloe Donegan <chloedonegan@gmail.com>
2015-08-01 12:55:21 +0100

    Model for list Items and associated migration

--- functional_tests.py
+++ functional_tests.py
@@ -12,30 +12,39 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
 
-        self.assertIn('Books', self.browser.title)
+        self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn("I've Read", header_text)
+        self.assertIn("Your To-Do list", header_text)
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a book you have read'
+                inputbox.get_attribute('placeholder'),
+                'Enter a to-do item'
         )
 
-        inputbox.send_keys('Shantaram')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == "1. Shantaram" for row in rows),
-            "New book did not appear in table"
-        )
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
+        # Edith wonders whether the site will remember her list. Then she sees
+        # that the site has generated a unique URL for her  there is some
+        # explanatory text to that effect.
         self.fail('Finish the test!')
+
+        # Satisfied, she goes back to sleep
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
-
