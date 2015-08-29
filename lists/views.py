27fcf81ba203b3804896c8c9d4977f12699d8906
Chloe Donegan <chloedonegan@gmail.com>
2015-08-29 08:38:21 +0100

    Use the form save method

--- lists/views.py
+++ lists/views.py
@@ -14,20 +14,10 @@ def home_page(request):
 # The items text will be the info that came
 # with text and it will be linked to the list. list=list_
 def new_list(request):
-    # list_ = List.objects.create()
-    # item = Item.objects.create(text=request.POST['text'], list=list_)
-    # try:
-    #     item.full_clean()
-    #     item.save()
-    # except ValidationError:
-    #     list_.delete()
-    #     error = "You can't have an empty list item"
-    #     return render(request, 'home.html', {"error": error})
-    # return redirect(list_)
     form = ItemForm(data=request.POST)
     if form.is_valid():
         list_ = List.objects.create()
-        Item.objects.create(text=request.POST['text'], list=list_)
+        form.save(for_list=list_)
         return redirect(list_)
     else:
         return render(request, 'home.html', {"form": form})
@@ -41,6 +31,6 @@ def view_list(request, list_id):
     if request.method == 'POST':
         form = ItemForm(data=request.POST)
         if form.is_valid():
-            Item.objects.create(text=request.POST['text'], list=list_)
+            form.save(for_list=list_)
             return redirect(list_)
     return render(request, 'list.html', {'list': list_, "form": form})
