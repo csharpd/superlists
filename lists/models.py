986c76599def12fc0bbbe7ab7efa531a267196f1
Chloe Donegan <chloedonegan@gmail.com>
2015-08-29 18:57:14 +0100

    more complicated django form

--- lists/models.py
+++ lists/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class List(models.Model):
     def get_absolute_url(self):
         return reverse('view_list', args=[self.id])
@@ -10,5 +11,12 @@ class List(models.Model):
 class Item(models.Model):
     text = models.TextField(default='')
     list = models.ForeignKey(List, default=None)
+
+    class Meta:
+        ordering = ('id',)
+        unique_together = ('list', 'text')
+
+    def __str__(self):
+        return self.text
 
 
