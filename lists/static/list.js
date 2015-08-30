fdb5d26fe24d80e0675ad4fa32f2b2e27202fa17
Chloe Donegan <chloedonegan@gmail.com>
2015-08-30 15:16:53 +0100

    chapter 15 user authentication part I

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,4 +1,11 @@
-$('input').on('keypress', function () {
-    $('.has-error').hide();
+jQuery(document).ready(function ($) {
+    $('input').on('keypress', function () {
+        $('.has-error').hide();
+    });
 });
+
+var loginLink = document.getElementById('id_login');
+if (loginLink) {
+  loginLink.onclick = function() { navigator.id.request(); };
+}
 
